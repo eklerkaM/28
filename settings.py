9dcf269a9e9c4372c7b3aeb3b49fa20c9b108a00
eklerkaM <129388602+eklerkaM@users.noreply.github.com>
2023-09-12 18:03:27 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,12 @@
-### параметры для авторизации
+import os
 
-valid_email = "nybys@yandex.ru"
-valid_phone = "+79265533627"
-valid_pass = "nY2023lip"
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+valid_email = os.getenv('valid_email')
+valid_password = os.getenv('valid_password')
+valid_login = os.getenv('valid_login')
+valid_phone = os.getenv('valid_phone')
+valid_pa = os.getenv('valid_pa')
